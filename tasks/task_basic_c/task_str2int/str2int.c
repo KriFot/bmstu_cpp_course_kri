f4a797fa94dbd5f1433df0543496c28eef438702
KriFot <nikita.arkhipenkov@gmail.com>
2024-10-12 02:01:03 +0300

    dolsno rabotat

--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -2,6 +2,42 @@
 #include "str2int.h"
 #include "stdio.h"
 
+//proverka na pustoty
+int is_space(char c) {
+    return (c == ' ');
+}
+
+//proverka na ot 0 do 9
+int is_digit(char c) {
+    return(c>='0' && c<='9');
+}
+
 int str2int(const char *str) {
-    return 0;
+
+    assert(str != NULL && ""); // proverka na 
+
+    int result = 0;
+    int sign = 1;
+    int max_int = 2147483647;
+    int min_int = -2147483647;
+    while (*str != '\0' && is_space(*str)){
+        str++;
+    }
+    if (*str == '-' || *str =='+'){
+        sign = (*str == '-') ? -1 : 1;
+        str++;
+    }
+    while (*str != '\0' && is_digit(*str)){
+        int digit = *str - '0';
+
+        assert(result > (max_int - digit)/10 && "");
+        
+        result = result * 10 + digit;
+        str++;
+    }
+    if (result * sign > max_int || result * sign < min_int){
+        assert(0 && "");
+    } 
+
+    return result * sign;
 }
